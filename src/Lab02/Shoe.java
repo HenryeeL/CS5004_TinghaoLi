ae3c00ceff833f48a991eb0b095dcd39d0d6751e
HenryeeL <l853490206@qq.com>
2024-01-24 15:47:18 -0800

    Lab02 & Javadoc

---
+++ src/Lab02/Shoe.java
@@ -0,0 +1,45 @@
+/**
+ * Represents a shoe with attributes: type, color, brand, and size.
+ */
+public class Shoe {
+    private String type;
+    private String color;
+    private String brand;
+    private double size;
+
+    /**
+     * Constructor for Shoe.
+     *
+     * @param type  Shoe type (e.g., "sneakers", "boots")
+     * @param color Shoe color
+     * @param brand Shoe brand
+     * @param size  Shoe size
+     */
+    public Shoe(String type, String color, String brand, double size) {
+        this.type = type;
+        this.color = color;
+        this.brand = brand;
+        this.size = size;
+    }
+
+    // Getters
+    public String getType() { return type; }
+    public String getColor() { return color; }
+    public String getBrand() { return brand; }
+    public double getSize() { return size; }
+
+    /**
+     * Returns string representation of Shoe.
+     *
+     * @return Shoe as string
+     */
+    @Override
+    public String toString() {
+        return "Shoe{" +
+                "type='" + type + '\'' +
+                ", color='" + color + '\'' +
+                ", brand='" + brand + '\'' +
+                ", size=" + size +
+                '}';
+    }
+}
